d907aeabd7151ff4e8c0b56e5a79e4f96685f37a
D-Beech <Datubeechmusic@gmail.com>
2024-09-05 10:09:41 +1200

    added settings for aws in codebase

--- index.js
+++ index.js
@@ -3,8 +3,14 @@ const mongoose = require('mongoose');
  
 const usersRouter = require('./routes/userAPI');
  
-require('dotenv').config(); //for using variables from .env file.
+// require('dotenv').config(); //for using variables from .env file.
+require('./config/config');
+
+console.log(process.env.NODE_ENV);
  
+console.log(process.env.MONGODB_URL);
+
+
 // Set up the express app
 const app = express(); //instantiates the express object
 const port = process.env.PORT || 3000; //app runs on localhost:3000 in the development environment
@@ -25,3 +31,25 @@ app.get('/', (req, res) => {
 });
 
 app.use('/users', usersRouter);
+
+app.listen(port, () => {
+  console.log(`Server is listening on port ${port}`);
+});
+ 
+//This enabled CORS, Cross-origin resource sharing (CORS) is a mechanism that allows restricted resources (e.g. fonts) 
+//on a web page to be requested from another domain outside the domain from which the first resource was served
+ 
+app.all('/*', function(req, res, next) {
+  // CORS headers
+  res.header("Access-Control-Allow-Origin", "*"); // restrict it to the required domain
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  // Set custom headers for CORS
+  res.header('Access-Control-Allow-Headers', 'Content-type,Accept,X-Access-Token,X-Key');
+  if (req.method == 'OPTIONS') {
+    res.status(200).end();
+  } else {
+    next();
+  }
+});
+ 
+module.exports = app;
